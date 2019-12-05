e8534d1cd988aecced73e4b1a85c4d99f7f8da93
Nikita MacBook <n.chernikov@kingbird.ru>
2019-12-05 20:34:55 +0300

    CSD-1 start to realize navigation and model

---
+++ CarServiceDiary/scene/Scene.swift
@@ -0,0 +1,13 @@
+//
+//  Scene.swift
+//  CarServiceDiary
+//
+//  Created by Никита Черников on 05/12/2019.
+//  Copyright © 2019 Никита Черников. All rights reserved.
+//
+
+import Foundation
+
+enum Scene {
+    case operationList(OperationListVM)
+}
